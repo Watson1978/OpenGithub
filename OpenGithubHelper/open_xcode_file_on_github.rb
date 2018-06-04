82946e351ddd45c9aa733275dfeab6c82d7f85ea
Watson <watson1978@gmail.com>
2018-06-05 01:49:16 +0900

    ensure to open origin file
    If add remote repository with `git remote add`, it might open file at non-origin.

--- OpenGithubHelper/open_xcode_file_on_github.rb
+++ OpenGithubHelper/open_xcode_file_on_github.rb
@@ -2,24 +2,27 @@ require_relative 'xcode_helper'
 
 xcode = XcodeHelper.new
 
-repository = nil
+remote = nil
 branch = nil
+repository = nil
 Dir.chdir xcode.project_root do
+  `git branch -r`.strip.lines do |line|
+    if line =~ %r{.+/HEAD -> (.+)/(.+)$}
+      remote = $1
+      branch = $2
+      break
+    end
+  end
+  exit unless remote
+  exit unless branch
+
   `git remote -v`.strip.lines do |line|
-    if line =~ %r{git@github.com:(.+)\.git \(fetch\)$} || line =~ %r{https://github.com/(.+)\.git \(fetch\)$}
+    if line =~ %r{#{remote}\s+git@github.com:(.+)\.git \(fetch\)$} || line =~ %r{#{remote}\s+https://github.com/(.+)\.git \(fetch\)$}
       repository = $1
       break
     end
   end
   exit unless repository
-
-  `git branch -r`.strip.lines do |line|
-    if line =~ %r{.+/HEAD -> .+/(.+)$}
-      branch = $1
-      break
-    end
-  end
-  exit unless branch
 end
 
 line = ARGV[0] || ""
