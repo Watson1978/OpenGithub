d6d0076f0b4a99385607c131bd05ae3231e71708
Watson <watson1978@gmail.com>
2018-06-05 01:18:51 +0900

    refactor

--- OpenGithubHelper/open_xcode_file_on_github.rb
+++ OpenGithubHelper/open_xcode_file_on_github.rb
@@ -1,52 +1,10 @@
-def detect_project_root(path)
-  path = File.dirname(path)
+require_relative 'xcode_helper'
 
-  if Dir.glob(File.join(path, ".git")).count > 0
-    return path
-  end
-
-  return nil if path == "/"
-  detect_project_root(path)
-end
-
-full_path = `osascript<<END
-  tell application "Xcode"
-    set file_name to name of front window
-    set file_name to replace(file_name, " — Edited", "") of me
-    
-    set num to number of source document
-    
-    repeat with index from 0 to num
-      set file_path to path of source document index
-      set pos to offset of file_name in file_path
-      
-      if pos is greater than 0 then
-        return file_path
-      end if
-    end repeat
-  end tell
-
-  on replace(orgStr, tgtStr, newStr)
-    
-    local orgDelim, rtn
-    
-    set orgDelim to AppleScript's text item delimiters
-    set AppleScript's text item delimiters to {tgtStr}
-    set rtn to every text item of orgStr
-    set AppleScript's text item delimiters to {newStr}
-    set rtn to rtn as string
-    set AppleScript's text item delimiters to orgDelim
-    return rtn
-    
-  end replace
-END`.strip
-
-project_root = detect_project_root(full_path)
-file_path = full_path.sub("#{project_root}/", '')
+xcode = XcodeHelper.new
 
 repository = nil
 branch = nil
-Dir.chdir project_root do
+Dir.chdir xcode.project_root do
   `git remote -v`.strip.lines do |line|
     if line =~ %r{git@github.com:(.+)\.git \(fetch\)$} || line =~ %r{https://github.com/(.+)\.git \(fetch\)$}
       repository = $1
@@ -69,4 +27,4 @@ unless line.empty?
   line = "#" + line
 end
 
-system "open 'https://github.com/#{repository}/blob/#{branch}/#{file_path}#{line}'"
+system "open 'https://github.com/#{repository}/blob/#{branch}/#{xcode.file_path}#{line}'"
---
+++ OpenGithubHelper/xcode_helper.rb
@@ -0,0 +1,57 @@
+class XcodeHelper
+  def full_path
+    @full_path ||= `osascript<<END
+      tell application "Xcode"
+        set file_name to name of front window
+        set file_name to replace(file_name, " — Edited", "") of me
+        
+        set num to number of source document
+        
+        repeat with index from 0 to num
+          set file_path to path of source document index
+          set pos to offset of file_name in file_path
+          
+          if pos is greater than 0 then
+            return file_path
+          end if
+        end repeat
+      end tell
+
+      on replace(orgStr, tgtStr, newStr)
+        
+        local orgDelim, rtn
+        
+        set orgDelim to AppleScript's text item delimiters
+        set AppleScript's text item delimiters to {tgtStr}
+        set rtn to every text item of orgStr
+        set AppleScript's text item delimiters to {newStr}
+        set rtn to rtn as string
+        set AppleScript's text item delimiters to orgDelim
+        return rtn
+        
+      end replace
+    END`.strip
+  end
+
+  def project_root
+    if @project_root.nil?
+      @project_root = File.dirname(full_path)
+
+      loop do
+        return @project_root if File.exist?(File.join(@project_root, ".git"))
+
+        @project_root = File.dirname(@project_root)
+        if @project_root == "/"
+          @project_root = nil
+          return @project_root
+        end
+      end
+    end
+
+    @project_root
+  end
+
+  def file_path
+    full_path.sub("#{project_root}/", '')
+  end
+end
